83cf08ca96ccf20a42b373ef0b66f350239224f8
jackzhoumine <jackzhoumine@163.com>
2019-01-01 02:26:09 +0800

    8.4. 过滤帖子列表

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,20 +1,24 @@
 import Vue from 'vue'
 import validator from './validator'
 import dropdown from './dropdown'
+// 引入 title.js 的默认值
+import title from './title'
 //注册全局指令，所有组件都能用
 /**
  *'validator'———— 指令名
  * validator ———— 指令对象
  */
-Vue.directive('validator', validator)
-Vue.directive('dropdown', dropdown)
+/* Vue.directive('validator', validator)
+Vue.directive('dropdown', dropdown) */
 
 //使用循环来注册指令
-/* const directives = {
-  validator,
-  dropdown
+const directives = {
+    validator,
+    dropdown,
+    // 添加 title 以便在循环中进行注册
+    title
 }
 
 for (const [key, value] of Object.entries(directives)) {
-  Vue.directive(key, value)
-} */
+    Vue.directive(key, value)
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,15 @@
             <div class="panel pane-default">
                 <div class="panel-heading">
                     <ul class="list-inline topic-filter">
-                        <li> <a href="/topics?filter=defalut" class="active">活跃</a></li>
+                        <!-- <li> <a href="/topics?filter=defalut" class="active">活跃</a></li>
                         <li> <a href="/topics?filter=excellent">精华</a></li>
                         <li> <a href="/topics?filter=vote">投票</a></li>
                         <li> <a href="/topics?filter=recent">最近</a></li>
-                        <li> <a href="/topics?filter=noreply">零回复</a></li>
+                        <li> <a href="/topics?filter=noreply">零回复</a></li> -->
+                        <li v-for="item in filters">
+                            <router-link v-title="item.title" :class="{ active: filter === item.filter }" :to="`/topics?filter=${item.filter}`">{{
+                                item.name }}</router-link>
+                        </li>
                     </ul>
                     <!-- 防止父级元素塌陷 -->
                     <div class="clearfix"></div>
@@ -54,7 +58,19 @@
             return {
                 msg: '', // 消息
                 msgType: '', // 消息类型
-                msgShow: false // 是否显示消息，默认不显示
+                msgShow: false, // 是否显示消息，默认不显示
+                articles: [],
+                filter: 'default',
+                /*
+                列表数据添加到数据对象，而不是直接写在模板里，这样做不仅可以简化模板，还可以方便更改逻辑。
+                */
+                filters: [
+                    { filter: 'default', name: '活跃', title: '最后回复排序' },
+                    { filter: 'excellent', name: '精华', title: '只看加精的话题' },
+                    { filter: 'vote', name: '投票', title: '点赞数排序' },
+                    { filter: 'recent', name: '最近', title: '发布时间排序' },
+                    { filter: 'noreply', name: '零回复', title: '无人问津的话题' },
+                ]
             }
         },
         /**
@@ -96,6 +112,8 @@
                 } else if (logout) {
                     vm.showMsg('操作成功')
                 }
+                // 确认渲染该组件的对应路由时，设置相关数据
+                vm.setDataByFilter(to.query.filter)
             })
         },
         // TODO:计算属性只有一个状态，一个计算属性就覆盖了整个计算属性
@@ -115,16 +133,18 @@
             //otherComputed
             //  推荐这种写法 添加其他计算属性方便
             ...mapState(['auth', 'user']),
-            //用派生状态作为所有文章
-            articles(){
-                return this.$store.getters.computedArticles
-            }
         },
         watch: {
             auth(value) {
                 if (!value) {
                     this.showMsg('操作成功')
                 }
+            },
+            /*
+            当路由从 /topics?filter=default 跳到类似 /topics?filter=vote 的地址时，Home.vue 会被复用，beforeRouteEnter 不会被再次调用，此时，我们需要监听 '$route' 以根据路由参数的变化，重新获取对应数据。
+            */
+            '$route'(to) {
+                this.setDataByFilter(to.query.filter)
             }
         },
         methods: {
@@ -132,6 +152,10 @@
                 this.msg = msg
                 this.msgType = type
                 this.msgShow = true
+            },
+            setDataByFilter(filter = 'default') {
+                this.filter = filter
+                this.articles = this.$store.getters.getArticlesByFilter(filter)
             }
         }
     }
