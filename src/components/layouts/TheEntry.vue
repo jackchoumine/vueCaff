299b18fe4063767c65896f21d47ac55b94ca1393
jackchou <jackzhou4job@163.com>
2021-01-17 19:06:23 +0800

    格式化代码

--- src/components/layouts/TheEntry.vue
+++ src/components/layouts/TheEntry.vue
@@ -1,64 +1,68 @@
 <!-- 个人入口菜单 -->
 <template>
-    <div class="navbar-right">
-        <ul v-if="auth" class="nav navbar-nav github-login">
-            <!-- 创作文章 -->
-            <li>
-                <a v-dropdown href="javascript:;">
-                    <i class="fa fa-plus text-md"></i>
-                </a>
-                <ul class="dropdown-menu">
-                    <li>
-                        <router-link to="/articles/create">
-                            <i class="fa fa-paint-brush text-md"></i>
-                            创作文章
-                        </router-link>
-                    </li>
-                </ul>
-            </li>
-            <!-- 登录状态 -->
-            <li>
-                <a v-dropdown href="javascript:;">
-                    <span v-if="user">
-                        <img v-if="user.avatar" :src="user.avatar" class="avatar-topnav">
-                        <span v-if="user.name">{{ user.name }}</span>
-                    </span>
-                    <span v-else>佚名</span>
-                    <span class="caret"></span>
-                </a>
-                <!-- 入口菜单 -->
-                <ul class="dropdown-menu">
-                    <!-- 个人专栏 -->
-                    <li v-if="user">
-                        <router-link :to="`/${user.name}`">
-                            <i class="fa fa-list-ul text-md i-middle"></i>
-                            个人专栏
-                        </router-link>
-                    </li>
-                    <!-- 编辑资料 -->
-                    <li>
-                        <router-link to="/users/1/edit">
-                            <i class="fa fa-cog text-md i-middle"></i>
-                            编辑资料
-                        </router-link>
-                    </li>
-                    <!-- 退出 -->
-                    <li>
-                        <a href="#" @click="logout">
-                            <i class="fa fa-sign-out text-md"></i>退出</a>
-                    </li>
-                </ul>
-            </li>
+  <div class="navbar-right">
+    <ul v-if="auth" class="nav navbar-nav github-login">
+      <!-- 创作文章 -->
+      <li>
+        <a v-dropdown href="javascript:;">
+          <i class="fa fa-plus text-md"></i>
+        </a>
+        <ul class="dropdown-menu">
+          <li>
+            <router-link to="/articles/create">
+              <i class="fa fa-paint-brush text-md"></i>
+              创作文章
+            </router-link>
+          </li>
         </ul>
-        <div v-else class="nav navbar-nav github-login">
-            <router-link to="/auth/login" class="btn btn-default login-btn">
-                <i class="fa fa-user"></i> 登 录
+      </li>
+      <!-- 登录状态 -->
+      <li>
+        <a v-dropdown href="javascript:;">
+          <span v-if="user">
+            <img v-if="user.avatar" :src="user.avatar" class="avatar-topnav" />
+            <span v-if="user.name">{{ user.name }}</span>
+          </span>
+          <span v-else>佚名</span>
+          <span class="caret"></span>
+        </a>
+        <!-- 入口菜单 -->
+        <ul class="dropdown-menu">
+          <!-- 个人专栏 -->
+          <li v-if="user">
+            <router-link :to="`/${user.name}`">
+              <i class="fa fa-list-ul text-md i-middle"></i>
+              个人专栏
             </router-link>
-            <router-link to="/auth/register" class="btn btn-default login-btn">
-                <i class="fa fa-user-plus"></i> 注 册
+          </li>
+          <!-- 编辑资料 -->
+          <li>
+            <router-link to="/users/1/edit">
+              <i class="fa fa-cog text-md i-middle"></i>
+              编辑资料
             </router-link>
-        </div>
+          </li>
+          <!-- 退出 -->
+          <li>
+            <a href="#" @click="logout">
+              <i class="fa fa-sign-out text-md"></i>
+              退出
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <div v-else class="nav navbar-nav github-login">
+      <router-link to="/auth/login" class="btn btn-default login-btn">
+        <i class="fa fa-user"></i>
+        登 录
+      </router-link>
+      <router-link to="/auth/register" class="btn btn-default login-btn">
+        <i class="fa fa-user-plus"></i>
+        注 册
+      </router-link>
     </div>
+  </div>
 </template>
 
 <script>
@@ -67,7 +71,7 @@ import { mapState } from 'vuex'
 export default {
   name: 'TheEntry',
   computed: {
-    ...mapState(['auth', 'user'])
+    ...mapState(['auth', 'user']),
     //不使用 mapState
     /*  auth() {
       return this.$store.state.auth
@@ -81,12 +85,12 @@ export default {
     logout() {
       this.$swal({
         text: '你确定要退出吗？',
-        confirmButtonText: '退出'
+        confirmButtonText: '退出',
       }).then(res => {
         if (res.value) this.$store.dispatch('logout')
       })
-    }
-  }
+    },
+  },
 }
 </script>
 
--- src/components/layouts/TheFooter.vue
+++ src/components/layouts/TheFooter.vue
@@ -5,10 +5,10 @@
       <div class="row footer-top">
         <div class="col-sm-5 col-lg-5">
           <p class="padding-top-xsm">{{ description }}</p>
-
           <div class="text-md">
             <a
               v-for="item in contacts"
+              :key="item.link"
               :title="item.title"
               :href="item.link"
               :style="contactStyle"
@@ -17,9 +17,7 @@
               <i :class="`fa fa-${item.icon}`"></i>
             </a>
           </div>
-
           <br />
-
           <span v-html="designer"></span>
         </div>
         <!--col-lg-offset-1 右移一栏-->
@@ -30,7 +28,7 @@
               <h4>{{ sponsor.title }}</h4>
 
               <ul class="list-unstyled">
-                <li v-for="item in sponsor.list">
+                <li v-for="(item, index) in sponsor.list" :key="index">
                   <a :href="item.link" target="_blank">
                     <img
                       v-title="item.title"
@@ -48,20 +46,18 @@
               <h4>{{ statistics.title }}</h4>
 
               <ul class="list-unstyled">
-                <li v-for="item in statistics.list">
-                  {{ item.title }}: {{ item.description }}
-                </li>
+                <li v-for="(item, index) in statistics.list" :key="index">{{ item.title }}: {{ item.description }}</li>
               </ul>
             </div>
             <!-- 其他信息 -->
             <div class="col-sm-4">
               <h4>{{ other.title }}</h4>
-
               <ul class="list-unstyled">
-                <li v-for="item in other.list">
+                <li v-for="(item, index) in other.list" :key="index">
                   <a :href="item.link" :title="item.title" target="_blank">
                     <!-- 用模板字符串更新部分数据 -->
-                    <i :class="`fa fa-${item.icon}`"></i> {{ item.title }}
+                    <i :class="`fa fa-${item.icon}`"></i>
+                    {{ item.title }}
                   </a>
                 </li>
               </ul>
@@ -74,38 +70,36 @@
 </template>
 
 <script>
-import title from "@/directives/title";
+import title from '@/directives/title'
 export default {
-  name: "TheFooter",
+  name: 'TheFooter',
   //   自定义指令 局部注册，只能在 TheFooter 组件上可用
   directives: {
     title,
   },
   data() {
     return {
-      description: "VuejsCaff 是一个 Vue.js 的知识社区",
+      description: 'VuejsCaff 是一个 Vue.js 的知识社区',
       contacts: [
         {
-          icon: "envelope",
-          title: "反馈问题",
-          link: "mailto:summer@yousails.com",
+          icon: 'envelope',
+          title: '反馈问题',
+          link: 'mailto:summer@yousails.com',
         },
         {
-          icon: "weibo",
-          title: "站长微博",
-          link: "https://weibo.com/1837553744/profile?topnav=1&wvr=6",
+          icon: 'weibo',
+          title: '站长微博',
+          link: 'https://weibo.com/1837553744/profile?topnav=1&wvr=6',
         },
         {
-          icon: "weixin",
-          title: "加我微信",
-          link: "https://vuejscaff.com/contact",
+          icon: 'weixin',
+          title: '加我微信',
+          link: 'https://vuejscaff.com/contact',
         },
       ],
-      // 样式 绑定到  :style 特性，样式属性采用小驼峰式
       contactStyle: {
-        paddingRight: "8px",
+        paddingRight: '8px',
       },
-      //   模板字符串
       designer: `
         <span style="font-size:0.9em">Designed by
           <span style="color: #e27575;font-size: 14px;">❤</span>
@@ -113,72 +107,68 @@ export default {
         </span>
       `,
       sponsor: {
-        title: "赞助商",
+        title: '赞助商',
         list: [
           {
-            logo:
-              "https://lccdn.phphub.org/uploads/banners/bQawWl3vT5dc2lYx5JZ7.png",
-            title: "本站服务器由 UCloud 赞助",
+            logo: 'https://laravel-china.org/uploads/banners/bQawWl3vT5dc2lYx5JZ7.png',
+            title: '本站服务器由 UCloud 赞助',
             link:
-              "http://www.ucloud.cn/?utm_source=zanzhu&utm_campaign=phphub&utm_medium=display&utm_content=yejiao&ytag=phphubyejiao",
+              'https://laravel-china.org/?utm_source=zanzhu&utm_campaign=phphub&utm_medium=display&utm_content=yejiao&ytag=phphubyejiao',
           },
           {
-            logo:
-              "https://lccdn.phphub.org/uploads/banners/yGLIR0idW7zsInjsNmzr.png",
-            title: "本站 CDN 服务由七牛赞助",
-            link: "http://www.qiniu.com/?utm_source=phphub",
+            logo: 'https://laravel-china.org/uploads/banners/yGLIR0idW7zsInjsNmzr.png',
+            title: '本站 CDN 服务由七牛赞助',
+            link: 'http://www.qiniu.com/?utm_source=phphub',
           },
           {
-            logo:
-              "https://lccdn.phphub.org/uploads/banners/XPtLlZmIN1cQbLuDFEON.png",
-            title: "Composer 镜像赞助商",
-            link: "https://www.upyun.com/",
+            logo: 'https://laravel-china.org/uploads/banners/XPtLlZmIN1cQbLuDFEON.png',
+            title: 'Composer 镜像赞助商',
+            link: 'https://www.upyun.com/',
           },
           {
-            logo:
-              "https://lccdn.phphub.org/uploads/banners/JpTCK6OKYBIrBIWdtob8.png",
-            title: "订阅邮件赞助商：SendCloud",
-            link: "http://www.sendcloud.net/",
+            logo: 'https://laravel-china.org/uploads/banners/JpTCK6OKYBIrBIWdtob8.png',
+            title: '订阅邮件赞助商：SendCloud',
+            link: 'http://www.sendcloud.net/',
           },
         ],
       },
       statistics: {
-        title: "统计信息",
+        title: '统计信息',
         list: [
           {
-            title: "社区会员",
-            description: "22889",
+            title: '社区会员',
+            description: '22889',
           },
           {
-            title: "话题数",
-            description: "7397",
+            title: '话题数',
+            description: '7397',
           },
           {
-            title: "评论数",
-            description: "39375",
+            title: '评论数',
+            description: '39375',
           },
         ],
       },
       other: {
-        title: "其他信息",
+        title: '其他信息',
         list: [
           {
             //   点赞图标
-            icon: "thumbs-up",
-            title: "软件外包服务",
-            link: "https://vuejscaff.com/contact",
+            icon: 'thumbs-up',
+            title: '软件外包服务',
+            link: 'https://vuejscaff.com/contact',
           },
           {
             //   地球图标
-            icon: "globe",
-            title: "推荐网站",
-            link: "https://vuejscaff.com/sites",
+            icon: 'globe',
+            title: '推荐网站',
+            link: 'https://vuejscaff.com/sites',
           },
         ],
       },
-    };
+    }
   },
-};
+}
 </script>
 
 <style scoped>
