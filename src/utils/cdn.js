dc372a9b84763a9e6298170216057e7a83a60ff1
jackchou <jackzhou4job@163.com>
2021-01-17 19:14:44 +0800

    配置cdn前缀

---
+++ src/utils/cdn.js
@@ -0,0 +1,8 @@
+/*
+ * @Description: cdn 前缀
+ * @Date: 2021-01-17 19:09:26 +0800
+ * @Author: JackChou
+ * @LastEditTime: 2021-01-17 19:12:46 +0800
+ * @LastEditors: JackChou
+ */
+export const cdn = 'https://laravel-china.org'
